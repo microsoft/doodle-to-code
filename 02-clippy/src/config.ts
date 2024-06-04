106ce22b9d901a06519d5bb89893a17c1cb5702a
Ayca Bas <36196437+aycabas@users.noreply.github.com>
2024-06-04 15:50:42 +0200

    clippy

---
+++ 02-clippy/src/config.ts
@@ -0,0 +1,9 @@
+const config = {
+  botId: process.env.BOT_ID,
+  botPassword: process.env.BOT_PASSWORD,
+  azureOpenAIKey: process.env.AZURE_OPENAI_API_KEY,
+  azureOpenAIEndpoint: process.env.AZURE_OPENAI_ENDPOINT,
+  azureOpenAIDeploymentName: process.env.AZURE_OPENAI_DEPLOYMENT_NAME,
+};
+
+export default config;
